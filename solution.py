70130e16ca6f92dd81dabda1735feab22e875a12
BkabirB <70675271+BkabirB@users.noreply.github.com>
2020-11-06 00:47:48 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -113,6 +113,10 @@ def ping(host, timeout=1):
     print("Pinging " + dest + " using Python:")
     print("")
     # Calculate vars values and return them
+    packet_min=min(packet)
+    packet_avg=sum(packet)/len(packet)
+    packet_max=max(packet)
+   
     vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(stdev(stdev_var), 2))]
     # Send ping requests to a server separated by approximately one second
     for i in range(0,4):
